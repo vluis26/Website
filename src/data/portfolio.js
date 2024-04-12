bce222fda689623c824a60f9a9e387c4d2c8a59c
Luis Villa <vluis@ad.unc.edu>
2024-04-11 23:06:53 -0400

    commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,14 +1,14 @@
 export default [
     {
         title: "Akari",
-        imgUrl: "/assets/Akari.jpeg",
+        imgUrl: "/Website/assets/Akari.jpeg",
         description: "Replica of the game Akari.",
         stack: ["Java", "JavaFX"],
     },
 
     {
         title: "Expense Tracker",
-        imgUrl: "/assets/tracker.jpeg",
+        imgUrl: "/Website/assets/tracker.jpeg",
         description: "A way to have financial management.",
         stack: ["MongoDB", "Express.js","React","Node.js"],
     },
